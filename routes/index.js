f31a321c2410454d50c190f9992f137ad36375ae
Elijah5399 <chia.elijah@gmail.com>
2023-05-18 00:41:00 +0800

    changed routing

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,37 @@ router.get("/", function (req, res, next) {
   }
 });
 
+router.get("/topic1", function (req, res, next) {
+  if (!req.user) {
+    res.locals.user = null;
+    res.render("index");
+  } else {
+    //this is necessary to define user in index.ejs!!!!
+    res.locals.user = req.user;
+    res.render("index");
+  }
+});
+
+router.get("/topic2", function (req, res, next) {
+  if (!req.user) {
+    res.locals.user = null;
+    res.render("index");
+  } else {
+    //this is necessary to define user in index.ejs!!!!
+    res.locals.user = req.user;
+    res.render("index");
+  }
+});
+
+router.get("/topic3", function (req, res, next) {
+  if (!req.user) {
+    res.locals.user = null;
+    res.render("index");
+  } else {
+    //this is necessary to define user in index.ejs!!!!
+    res.locals.user = req.user;
+    res.render("index");
+  }
+});
+
 module.exports = router;
