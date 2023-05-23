64407e61ce577872d5de16000b85ab02c5f51b6b
Quasant <benjaminthechin@gmail.com>
2023-05-23 17:26:52 +0800

    Polished navbar, BUT I FORGOT TO PULL

--- views/script.js
+++ views/script.js
@@ -18,3 +18,25 @@ function contact() {}
 
 
 
+const wrapper = document.querySelector('.wrapper')
+const rightContainer = document.getElementById('right-container')
+
+function openSlideMenu() {
+  wrapper.style.width = "250px";
+  rightContainer.style.marginLeft = "250px";
+}
+function closeSlideMenu() {
+  wrapper.style.width = "0";
+  rightContainer.style.marginLeft = "0";
+}
+
+const mediaQuery = window.matchMedia('(min-width: 768px)')
+if (mediaQuery.matches) {
+  openSlideMenu();
+  wrapper.style.transition = "width 0.3s";
+  rightContainer.style.transition = "margin-left 0.3s";
+} else {
+  closeSlideMenu();
+  wrapper.style.transition = "width 0.3s";
+  rightContainer.style.transition = "margin-left 0.3s"
+}
